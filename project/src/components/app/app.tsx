3540cce08dd2090487916ba86e7c2cad54ac429f
Andrey S <pesopi@mail.ru>
2022-07-14 18:17:59 +0300

    Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,50 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PropertyScreen from '../../pages/property-screen/property-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   offersCount: number;
 }
 
-function App({offersCount}: AppScreenProps): JSX.Element {
+function App({ offersCount }: AppScreenProps): JSX.Element {
   return (
-    <MainScreen offersCount={offersCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen offersCount={offersCount} />}
+        />
+
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path={AppRoute.Room}
+          element={<PropertyScreen />}
+        />
+
+        <Route
+          path='*'
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
