127c3681859ca45f4187ddced2ce0911e7559e13
Andrey S <pesopi@mail.ru>
2022-08-21 22:19:12 +0300

    Переносит загрузку всех приложений из index.tsx в MainScreen

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,24 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import { AppRoute } from '../../const';
-import { useAppSelector } from '../../hooks';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
-import LoadingScreen from '../../pages/loading-screen/loading-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import PropertyScreen from '../../pages/property-screen/property-screen';
-import { getIsDataLoadedStatus } from '../../store/app-data/selectors';
 import PrivateRoute from '../private-route/private-route';
 
 function App(): JSX.Element {
-  const isDataLoaded = useAppSelector(getIsDataLoadedStatus);
-
-  if (!isDataLoaded) {
-    return (
-      <LoadingScreen />
-    );
-  }
-
   return (
     <Routes>
       <Route
