5020716f4a1cde676078f385db7286f39da65452
Andrey S <pesopi@mail.ru>
2022-08-21 13:45:34 +0300

    Реализует вывод отзывов не больше 10, отсортированных от новых к старым

--- project/src/store/app-data/app-data.ts
+++ project/src/store/app-data/app-data.ts
@@ -7,6 +7,7 @@ import { fetchOffers } from '../api-actions';
 
 export const DEFAULT_CITY: City = LOCATIONS.find((location) => location.name === 'Paris') || LOCATIONS[0];
 export const DEFAULT_SORING_TYPE = SortingType.Popular;
+const NUMBER_OF_REVIEWS = 10;
 
 export const getInitialStateAppData = (): AppData => ({
   city: DEFAULT_CITY,
@@ -29,7 +30,16 @@ export const appData = createSlice({
       state.offers.push(action.payload);
     },
     storeReviews: (state, action: PayloadAction<Reviews>) => {
-      state.reviews = action.payload;
+      const reviews = Array.from(action.payload)
+        .sort((a, b) => {
+          const timeA = new Date(a.date).getTime();
+          const timeB = new Date(b.date).getTime();
+
+          return timeB - timeA;
+        })
+        .slice(0, NUMBER_OF_REVIEWS);
+
+      state.reviews = reviews;
     },
     storeNearbyOffers: (state, action: PayloadAction<Offers>) => {
       state.nearbyOffers = action.payload;
