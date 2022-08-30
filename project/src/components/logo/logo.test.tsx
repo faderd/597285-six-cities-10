bddd3a73eada6232ecd7feb02710f5d28d4db60d
Andrey S <pesopi@mail.ru>
2022-08-30 13:04:00 +0300

    Дорабатывает тест logo.test

--- project/src/components/logo/logo.test.tsx
+++ project/src/components/logo/logo.test.tsx
@@ -1,7 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
+import { Route, Routes } from 'react-router-dom';
 import HistoryRouter from '../history-router/history-router';
 import Logo from './logo';
+import userEvent from '@testing-library/user-event';
 
 const history = createMemoryHistory();
 
@@ -15,5 +17,29 @@ describe('Component: Logo', () => {
 
     expect(screen.getByAltText('6 cities logo')).toBeInTheDocument();
     expect(screen.getByRole('link')).toBeInTheDocument();
+  });
+
+  it('should redirect to root url when user clicked to link', async () => {
+    history.push('/fake');
+
+    render(
+      <HistoryRouter history={history}>
+        <Routes>
+          <Route
+            path='/'
+            element={<h1>This is main page</h1>}
+          />
+          <Route
+            path='*'
+            element={<Logo />}
+          />
+        </Routes>
+      </HistoryRouter>);
+
+    expect(screen.queryByText(/This is main page/i)).not.toBeInTheDocument();
+
+    await userEvent.click(screen.getByRole('link'));
+
+    expect(screen.getByText(/This is main page/i)).toBeInTheDocument();
   });
 });
